811a5dae3362b12c123294cf0fd008f748cbde85
Jessica Jones <daddehjones@gmail.com>
2021-04-02 22:44:50 -0400

    Add dinosaur facts

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+cool_ass_dinosaur = {
+  "name" => 'Escobar',
+  "skin" => 'Scaly',
+  "hight_in_feet" => 10,
+  "age" => 500,
+  "is_smart" => true,
+  "is_dead" => false,
+  "dinosaur_buddies" => [ 'Enrique', 'Pablo', 'Steve'],
+  "favorite_foods" => ['humans', 'veggies', 'cars']
+
+  }
