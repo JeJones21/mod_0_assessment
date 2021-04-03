8ff75464bd4d34ee2afa6164c6c6719f5d75b741
Jessica Jones <daddehjones@gmail.com>
2021-04-02 22:57:02 -0400

    Add developer list

---
+++ models/model_b.rb
@@ -0,0 +1,11 @@
+Developer = {
+  "name" => "JJ",
+  "hands" => "quick",
+  "age" => 34,
+  "wisdom_level" => 5000,
+  "is_intelligent" => true,
+  "is_unoccupied" => false,
+  "favorite_bevs" => ["tequila", "water", "whiskey"],
+  "favorite_websites" => ["youtube", "google", "github"]
+
+}
