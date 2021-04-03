adc6e854ed9b4a7ae4ea16e2fb8f2d3e808973c4
Jessica Jones <daddehjones@gmail.com>
2021-04-02 22:18:17 -0400

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,20 @@
+movie = 'Spacejam'
+is_entertaining = true
+movie = 'Peanut Butter Falcon'
+num_4 = 808
+num_6 = 555
+product = 448440
+favorite_foods = ['tacos', 'enchiladas', 'soup']
+favorite_foods.shift
+puts "movie:"
+strl=gets.chomp
+
+count=0
+for i in l..strl.length
+  if (strl[i] != '')
+    count+=1
+  end
+
+end
+
+puts "Number of characters are #{count}"
